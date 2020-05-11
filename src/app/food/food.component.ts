90f9ff67df6e823fbac99a678b3241eb9985b700
Erick Pereira da Silva <eriiickps@gmail.com>
2020-05-11 16:41:09 -0300

    Add food component

--- src/app/food/food.component.ts
+++ src/app/food/food.component.ts
@@ -6,10 +6,83 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./food.component.styl']
 })
 export class FoodComponent implements OnInit {
+  public snakeFoods = [];
+
+  private randomIntFromIntervalAndFrame(min: number, max: number, frame: number): number {
+    return Math.round((Math.random() * (max - min) + min) / frame) * frame;
+  }
+
+  private generateFoodInARandomPosition(): void {
+    const { offsetWidth, offsetHeight } = document.getElementById('field');
+    const frame = 8;
+
+    this.snakeFoods.push({
+      width: frame,
+      height: frame,
+      top: this.randomIntFromIntervalAndFrame(0, (offsetHeight - frame), frame),
+      left: this.randomIntFromIntervalAndFrame(0, (offsetWidth - frame), frame),
+    });
+    this.snakeFoods.push({
+      width: frame,
+      height: frame,
+      top: this.randomIntFromIntervalAndFrame(0, (offsetHeight - frame), frame),
+      left: this.randomIntFromIntervalAndFrame(0, (offsetWidth - frame), frame),
+    });
+    this.snakeFoods.push({
+      width: frame,
+      height: frame,
+      top: this.randomIntFromIntervalAndFrame(0, (offsetHeight - frame), frame),
+      left: this.randomIntFromIntervalAndFrame(0, (offsetWidth - frame), frame),
+    });
+    this.snakeFoods.push({
+      width: frame,
+      height: frame,
+      top: this.randomIntFromIntervalAndFrame(0, (offsetHeight - frame), frame),
+      left: this.randomIntFromIntervalAndFrame(0, (offsetWidth - frame), frame),
+    });
+    this.snakeFoods.push({
+      width: frame,
+      height: frame,
+      top: this.randomIntFromIntervalAndFrame(0, (offsetHeight - frame), frame),
+      left: this.randomIntFromIntervalAndFrame(0, (offsetWidth - frame), frame),
+    });
+    this.snakeFoods.push({
+      width: frame,
+      height: frame,
+      top: this.randomIntFromIntervalAndFrame(0, (offsetHeight - frame), frame),
+      left: this.randomIntFromIntervalAndFrame(0, (offsetWidth - frame), frame),
+    });
+    this.snakeFoods.push({
+      width: frame,
+      height: frame,
+      top: this.randomIntFromIntervalAndFrame(0, (offsetHeight - frame), frame),
+      left: this.randomIntFromIntervalAndFrame(0, (offsetWidth - frame), frame),
+    });
+    this.snakeFoods.push({
+      width: frame,
+      height: frame,
+      top: this.randomIntFromIntervalAndFrame(0, (offsetHeight - frame), frame),
+      left: this.randomIntFromIntervalAndFrame(0, (offsetWidth - frame), frame),
+    });
+    this.snakeFoods.push({
+      width: frame,
+      height: frame,
+      top: this.randomIntFromIntervalAndFrame(0, (offsetHeight - frame), frame),
+      left: this.randomIntFromIntervalAndFrame(0, (offsetWidth - frame), frame),
+    });
+    this.snakeFoods.push({
+      width: frame,
+      height: frame,
+      top: this.randomIntFromIntervalAndFrame(0, (offsetHeight - frame), frame),
+      left: this.randomIntFromIntervalAndFrame(0, (offsetWidth - frame), frame),
+    });
+
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    this.generateFoodInARandomPosition();
   }
 
 }
