b87823b4afa33af138ec23bcb3df6d615aa695a7
Erick Pereira da Silva <eriiickps@gmail.com>
2020-05-13 16:30:50 -0300

    reuse fieldSize from data service

--- src/app/field/field.component.ts
+++ src/app/field/field.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../service/data/data.service';
 
 @Component({
   selector: 'app-field',
@@ -7,13 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class FieldComponent implements OnInit {
-  private frame = 8;
-  width = this.frame * 100;
-  height = this.frame * 50;
+  public width: number;
+  public height: number;
 
-  constructor() { }
+  constructor(private data: DataService) { }
 
   ngOnInit(): void {
+    this.width = this.data.fieldSizeX;
+    this.height = this.data.fieldSizeY;
   }
 
 }
